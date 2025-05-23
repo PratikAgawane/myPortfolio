025e44e987b233ce0992bedd3674b9073f210e9b
Pratik Agawane <55141518+PratikAgawane@users.noreply.github.com>
2025-05-23 17:21:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+<<<<<<< HEAD
+document.addEventListener("DOMContentLoaded", function () {
+    alert("Welcome to my portfolio!");
+});
+
+=======
+document.addEventListener("DOMContentLoaded", function () {
+    alert("Welcome to my portfolio!");
+});
+
+>>>>>>> 415d5b5 (Initial commit for myportfolio project)
