2012b05184bd8c879dcb07eacda29f35f5415e3c
Pratik Agawane <55141518+PratikAgawane@users.noreply.github.com>
2025-05-24 13:44:33 +0530

    updated files

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
 document.addEventListener("DOMContentLoaded", function () {
     alert("Welcome to my portfolio!");
 });
 
-=======
-document.addEventListener("DOMContentLoaded", function () {
-    alert("Welcome to my portfolio!");
-});
-
->>>>>>> 415d5b5 (Initial commit for myportfolio project)
